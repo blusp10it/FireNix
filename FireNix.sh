b8ec258b17b35630ffb7892f4becbe274feeea5b
Krisan Alfa Timur <blusp10it@gmail.com>
2012-02-25 23:40:26 +0700

    0.3 Release 1

--- FireNix.sh
+++ FireNix.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 #------------------------------COPYRIGHT------------------------------#
-# Jika kamu ingin mengembangkan scrip ini, jangan hapus bagian ini
-# (C)opyRight by blusp10it
-# Versi 0.3 R1
+# FireNix Versi 0.3 R1 (Configure your network in interactive shell)
+# Copyright (C) 2012 Krisan Alfa Timur A.K.A blusp10it
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #------------------------------COPYRIGHT------------------------------#
 
 trap cleanup INT
